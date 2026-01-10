4434ac58fbed1f246081b5362748bcdd30614dbb
naveen0913 <udayagirinaveen135@gmail.com>
2026-01-11 00:45:40 +0530

    Added response mapping

--- src/main/java/com/task/management/controller/TaskController.java
+++ src/main/java/com/task/management/controller/TaskController.java
@@ -2,6 +2,7 @@ package com.task.management.controller;
 
 import com.task.management.model.Tasks;
 import com.task.management.repository.TaskRepository;
+import com.task.management.response.ApiResponse;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,26 +21,47 @@ public class TaskController {
     private final TaskRepository taskRepository;
 
     @PostMapping("/add")
-    public ResponseEntity<?> addTask(@RequestBody Tasks tasks) {
+    public ResponseEntity<ApiResponse<?>> addTask(@RequestBody Tasks tasks) {
         tasks.setCreatedAt(LocalDateTime.now());
-        taskRepository.save(tasks);
-        return ResponseEntity.status(HttpStatus.CREATED.value()).body(201);
+
+        Tasks savedTask = taskRepository.save(tasks);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new ApiResponse<>(
+                        201,
+                        "Task added successfully",
+                        null
+                ));
     }
 
     @GetMapping("/all")
-    public List<Tasks> getAllTasks() {
-        return taskRepository.findAll();
+    public ResponseEntity<ApiResponse<List<Tasks>>> getAllTasks() {
+        List<Tasks> tasks = taskRepository.findAll();
+
+        return ResponseEntity.ok(
+                new ApiResponse<>(
+                        200,
+                        "Tasks fetched successfully",
+                        tasks
+                )
+        );
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> markCompleted(@PathVariable Long id) {
+    public ResponseEntity<ApiResponse<?>> markCompleted(@PathVariable Long id) {
         Tasks tasks = taskRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Tasks not found"));
         tasks.setStatus(true);
         tasks.setUpdatedAt(LocalDateTime.now());
         taskRepository.save(tasks);
-        return ResponseEntity.status(HttpStatus.OK.value()).body(200);
-    }
+
+        return ResponseEntity.ok(
+                new ApiResponse<>(
+                        200,
+                        "Tasks Updated successfully",
+                        null
+                )
+        );    }
 
     @DeleteMapping("/delete/{id}")
     public Tasks deleteTasks(@PathVariable Long id) {
---
+++ src/main/java/com/task/management/response/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.task.management.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private int code;
+    private String message;
+    private T data;
+
+}
